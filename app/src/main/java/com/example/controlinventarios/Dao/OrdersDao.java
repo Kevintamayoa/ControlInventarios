3adc683ca05cde139c74357cfd2d77453ca990ef
Kevin Tamayo <kelpo_tamayo@hotmail.com>
2019-05-03 18:44:43 -0500

    gaurdado de info123132341234

--- app/src/main/java/com/example/controlinventarios/Dao/OrdersDao.java
+++ app/src/main/java/com/example/controlinventarios/Dao/OrdersDao.java
@@ -22,6 +22,19 @@ public interface OrdersDao {
     @Query("SELECT id as id,status_id as status_id,customer_id as customer_id,date as date,(case when change_log is null then ' ' else change_log end) as change_log FROM orders WHERE status_id IN (:list) AND customer_id=:customer_id")
     public List<Orders> getAllOrdersByStatusId(int[] list, int customer_id);
 
+    @Query(" SELECT * FROM orders o WHERE status_id IN (:list) AND date(substr(o.date,7,4)||'-'||substr(o.date,4,2)||'-'||substr(o.date,1,2))>=date(:DateIn)")
+    public List<Orders> getAllOrdersByDateIn(int[] list,String DateIn);
+    @Query(" SELECT * FROM orders o WHERE status_id IN (:list) AND date(substr(o.date,7,4)||'-'||substr(o.date,4,2)||'-'||substr(o.date,1,2))<=date(:DateIn)")
+    public List<Orders> getAllOrdersByDateEnd(int[] list,String DateIn);
+    @Query(" SELECT * FROM orders o WHERE status_id IN (:list) AND date(substr(o.date,7,4)||'-'||substr(o.date,4,2)||'-'||substr(o.date,1,2))<=date(:DateOut) AND date(substr(o.date,7,4)||'-'||substr(o.date,4,2)||'-'||substr(o.date,1,2))>=date(:DateIn)")
+    public List<Orders> getAllOrdersByDates(int[] list,String DateIn,String DateOut);
+    @Query(" SELECT * FROM orders o WHERE status_id IN (:list) AND date(substr(o.date,7,4)||'-'||substr(o.date,4,2)||'-'||substr(o.date,1,2))>=date(:DateIn) AND customer_id=:customer_id")
+    public List<Orders> getAllOrdersByDateInAndClient(int[] list,String DateIn, int customer_id);
+    @Query(" SELECT * FROM orders o WHERE status_id IN (:list) AND date(substr(o.date,7,4)||'-'||substr(o.date,4,2)||'-'||substr(o.date,1,2))<=date(:DateIn) AND customer_id=:customer_id")
+    public List<Orders> getAllOrdersByDateEndAndClient(int[] list,String DateIn, int customer_id);
+    @Query(" SELECT * FROM orders o WHERE status_id IN (:list) AND date(substr(o.date,7,4)||'-'||substr(o.date,4,2)||'-'||substr(o.date,1,2))<=date(:DateOut) AND date(substr(o.date,7,4)||'-'||substr(o.date,4,2)||'-'||substr(o.date,1,2))>=date(:DateIn) AND customer_id=:customer_id")
+    public List<Orders> getAllOrdersByDatesAndClient(int[] list,String DateIn,String DateOut, int customer_id);
+
     @Query("SELECT (CASE WHEN (SELECT SUM(A.qty) FROM order_assemblies A WHERE A.id= :id) IS NULL THEN 0 \n" +
             "ELSE (SELECT SUM(A.qty) FROM order_assemblies A WHERE A.id=:id) END) AS qty  ")
     public int getNumAssembliesByOrder(int id);
